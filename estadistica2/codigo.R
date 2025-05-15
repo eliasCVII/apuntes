13edfff8395b179ad7ded3a1f81f27841b905df9
elias <elias.contreras@usach.cl>
2025-05-14 21:03:19 -0400

    apuntesss

--- estadistica2/codigo.R
+++ estadistica2/codigo.R
@@ -5,7 +5,8 @@
 #   sigma DESVIACIÓN ESTÁNDAR POBLACIONAL
 #   nc NIVEL DE CONFIANZA
 
-ICMU1=function(n,prom,sigma,nc=0.95)
+# IC para mu con sigma, sigma2 conocido
+ICMU1=function(n,prom,sigma,nc=0.95) 
   {
     z=qnorm(1-(1-nc)/2)
     EE=z*sigma/sqrt(n)
@@ -23,6 +24,7 @@ ICMU1=function(n,prom,sigma,nc=0.95)
 #   s DESVIACIÓN ESTÁNDAR MUESTRAL
 #   nc NIVEL DE CONFIANZA
 
+# IC para mu con sigma, sigma2 desconocido
 ICMU2=function(n,prom,s,nc=0.95)
   {
     t=qt(1-(1-nc)/2,n-1)
@@ -40,6 +42,8 @@ ICMU2=function(n,prom,s,nc=0.95)
 #   s DESVIACIÓN ESTÁNDAR MUESTRAL
 #   nc NIVEL DE CONFIANZA
 
+
+# IC para sigma2 (varianza poblacional)
 ICSIGMA2=function(n,s,nc=0.95)
   {
     v1=qchisq((1-nc)/2,n-1)
@@ -57,6 +61,7 @@ ICSIGMA2=function(n,s,nc=0.95)
 #   p PROPORCIoN MUESTRAL
 #   nc NIVEL DE CONFIANZA
 
+# IC para pi (proporcion poblacional)
 ICPI=function(n,p,nc=0.95)
   {
     z=qnorm(1-(1-nc)/2)
@@ -74,6 +79,7 @@ ICPI=function(n,p,nc=0.95)
 #     sigma DESVIACIoN ESTaNDAR
 #
 
+# tamano de la muestra para media poblacional mu
 NMU=function(EE,nc,sigma)
   {
     z=qnorm(1-(1-nc)/2)
@@ -89,6 +95,7 @@ NMU=function(EE,nc,sigma)
 #     p PROPORCIoN CONOCIDA
 #
 
+# tamano de muestra para proporcion poblacional
 NPI=function(EE,nc,p)
   {
     z=qnorm(1-(1-nc)/2)
@@ -108,6 +115,8 @@ NPI=function(EE,nc,p)
 # sigma1, sigma2 DESVIACIONES POBLACIONALES
 # nc NIVEL DE CONFIANZA (PROPORCIÓN)
 
+# Diferencia de medias
+## Caso1. IC para dos poblaciones, con varianzas poblacionales conocidas
 difmed1=function(n1,n2,prom1,prom2,sigma1,sigma2,nc=0.95)
 {
   z=qnorm(1-(1-nc)/2)
@@ -126,7 +135,9 @@ difmed1=function(n1,n2,prom1,prom2,sigma1,sigma2,nc=0.95)
 # nc NIVEL DE CONFIANZA (PROPORCIÓN)
 #
 
-     difmed2=function(n1,n2,prom1,prom2,s1,s2,nc=0.95)
+# Diferencia de medias
+## Caso2. IC para dos poblaciones, con varianzas poblacionales desconocidas pero iguales
+difmed2=function(n1,n2,prom1,prom2,s1,s2,nc=0.95)
      {
         t=qt(1-(1-nc)/2,n1+n2-2)
   sp=sqrt(((n1-1)*s1^2+(n2-1)*s2^2)/(n1+n2-2))
@@ -145,7 +156,9 @@ difmed1=function(n1,n2,prom1,prom2,sigma1,sigma2,nc=0.95)
 # nc NIVEL DE CONFIANZA (PROPORCIÓN)
 #
 
-     difmed3=function(n1,n2,prom1,prom2,s1,s2,nc=0.95)
+# Diferencia de medias
+## Caso3. IC para dos poblaciones, con varianzas poblacionales desconocidas pero y diferentes
+difmed3=function(n1,n2,prom1,prom2,s1,s2,nc=0.95)
      {
   eta=(s1^2/n1+s2^2/n2)^2/((s1^2/n1)^2/(n1-1)+
              (s2^2/n2)^2/(n2-1))
@@ -164,6 +177,8 @@ difmed1=function(n1,n2,prom1,prom2,sigma1,sigma2,nc=0.95)
 # nc NIVEL DE CONFIANZA (PROPORCION)
 #
 
+# Diferencia de medias
+## Cuociente de varianzas poblacionales
 cuovar=function(n1,n2,s1,s2,nc=0.95)
      {
   f1=qf((1-nc)/2,n1-1,n2-1)
@@ -181,6 +196,7 @@ cuovar=function(n1,n2,s1,s2,nc=0.95)
 #   p1, p2 PROPORCIONES MUESTRALES
 #   nc NIVEL DE CONFIANZA (PROPORCIÓN)
 
+# IC para diferencia de proporciones pi1 - pi2
 difprop=function(n1,n2,p1,p2,nc=0.95){
   z=qnorm(1-(1-nc)/2)
   v1=p1*(1-p1)/n1
